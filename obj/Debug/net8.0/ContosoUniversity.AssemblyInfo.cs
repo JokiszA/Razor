89408fe935c3134c890f66460705f3fa27732cec
jokisza <adrian.jokisz@gmail.com>
2025-04-11 21:33:10 +0200

    courses

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff8ae0976a8e5b330af3cc99c2373a8a5c91ed8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e157093bd69d9a3f836e76476ff1f9ed56b88e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
