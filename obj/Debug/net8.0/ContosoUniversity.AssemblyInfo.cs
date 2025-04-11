316dc026e5c0f99794f173ef75df690593a33363
jokisza <adrian.jokisz@gmail.com>
2025-04-11 21:47:30 +0200

    part 7 update related data

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89408fe935c3134c890f66460705f3fa27732cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d98ad3a44c6bf3b69b99958cddb79e2601aa5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
