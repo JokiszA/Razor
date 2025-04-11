4930a52ff36e5edaae300520dc18c3afd46c08af
jokisza <adrian.jokisz@gmail.com>
2025-04-11 21:05:02 +0200

    paging

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5420906e9335a2fdd03e19ac8e052390e77cd67e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c29b68bbb852fdd9a4869284e5647a928065e18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
