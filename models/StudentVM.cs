b7af4239cd7ea3f4035ee029386ce5b2be1c981a
jokisza <adrian.jokisz@gmail.com>
2025-04-11 20:40:20 +0200

    studen vm part1

--- models/StudentVM.cs
+++ models/StudentVM.cs
@@ -1,13 +1,12 @@
+﻿using System;
+
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class StudentVM
     {
-        public int ID     { get; set; }
+        public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
-
-        public ICollection<Enrollment> Enrollments { get; set; }
-        public string Secret { get; set; }
     }
 }
